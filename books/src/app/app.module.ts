5171774dc21f0fbbf060b7770794b1fd1cb5f31a
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-13 00:21:31 +0300

    add not found page

--- books/src/app/app.module.ts
+++ books/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { BookPageComponent } from './components/book-page/book-page.component';
 import { CartPageComponent } from './components/cart-page/cart-page.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     BookPageComponent,
     CartPageComponent,
     BookDetailsComponent,
+    NotFoundComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
