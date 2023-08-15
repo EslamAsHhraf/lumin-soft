0dfa1605d16bdbfcd26e50dd565579123700dc14
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-15 13:00:57 +0300

    add to do controller

---
+++ Backend/ToDo/ToDoAPP/Controllers/TaskAPIController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoAPP.Data;
+using ToDoAPP.Models.DTO;
+
+
+namespace ToDoAPP.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskAPIController : ControllerBase
+    {
+       
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<IEnumerable<TaskDTO>> GetTasks()
+        {
+            return Ok(TaskStore.taskList);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult CompleteTask (int id, [FromBody] TaskDTO TaskDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (TaskDTO == null || id != TaskDTO.Id)
+            {
+                return BadRequest();
+            }
+            var task = TaskStore.taskList.FirstOrDefault(u => u.Id == id);
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+          
+            task.IsCompleted = TaskDTO.IsCompleted;
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Backend/ToDo/ToDoAPP/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPP.Models
+{
+    public class Task
+    {
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
